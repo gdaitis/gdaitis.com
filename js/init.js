c0286238ab09a4867d91eee0ec2cf1fae5932210
Vytautas Gudaitis <vytautas.gudaitis@gmail.com>
2015-10-27 17:00:01 +0200

    galleries completed

--- js/init.js
+++ js/init.js
@@ -4,5 +4,50 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  });
+})(jQuery);
+
+(function() {
+  lightbox.option({
+      'showImageNumberLabel': false
+    })
+}());
+
+$('.carousel-mobile').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+  infinite: true,
+  centerMode: true,
+  //centerPadding: '0px',
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+	
